d6bfeeb3562680d5b152cf5de98a7b8740a1fc6e
Ikkei <258lab@gmail.com>
2019-02-02 16:08:24 +0900

    test2

--- app/src/main/java/com/i258lab/test/MainActivity.java
+++ app/src/main/java/com/i258lab/test/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String a;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
